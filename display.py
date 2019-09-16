ad5619861ca86b7f7114d54c3c9fea81f0f47757
TheOldChainsaw <50226839+TheOldChainsaw@users.noreply.github.com>
2019-09-16 14:59:20 +0800

    Reverted changes for waypoints

--- display.py
+++ display.py
@@ -123,13 +123,6 @@ class Display():
 					self.screen.blit(marker, pos)
 				except Exception as e:
 					print(str(e))
-					
-			for wpoint in world_data['waypoints']:
-				try:
-					if wpoint is None:
-						continue
-					pygame.draw.circle(self.screen, (256, 0, 0), wpoint, 10, 1)
-				
 
 	def error_message(self, message):
 		self.screen.fill((0, 0, 0))
