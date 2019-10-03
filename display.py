c17b0e798d3c994f6aec47503f60250f65e28b6c
tt405rules <50159991+tt405rules@users.noreply.github.com>
2019-10-03 11:10:20 +0800

    Updated usability image
    This image is larger and it is located at the bottom of the menu screen. Hopefully it is less blurry

--- display.py
+++ display.py
@@ -5,8 +5,11 @@ import csv
 from os import listdir
 from os.path import isfile
 from constants import *
+import sys
+import datetime
 
 msgHeader = "[DISPLAY]: "
+gamepad = False
 
 
 class Display():
@@ -14,10 +17,15 @@ class Display():
         self.DEBUG = False
 
         pygame.init()
-        self.screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT), pygame.FULLSCREEN)
-        self.font = pygame.font.SysFont('Arial', int(DISPLAY_WIDTH / 50))
+        self.screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT), pygame.RESIZABLE)
         self.background_image = None
         self.default_text_position = (DISPLAY_WIDTH / 1.5, DISPLAY_HEIGHT / 1.2)
+        self.font_general = pygame.font.SysFont('Arial', int(DISPLAY_WIDTH / 50)) # Font for general text
+        self.font_timer = pygame.font.SysFont('Arial', int(DISPLAY_WIDTH / 30), True) # Font for timer text
+        self.font_timer2 = pygame.font.SysFont('Arial', int(DISPLAY_WIDTH / 50), True) # Font for secondary timer text
+        self.lap = False
+        self.race_complete = False
+        self.finish_time = None
 
         # Hide mouse.
         pygame.mouse.set_visible(False)
@@ -26,19 +34,20 @@ class Display():
         pygame.joystick.init()
         if (pygame.joystick.get_count() < 1):
             print(msgHeader + "No gamepad detected.")
-            exit()
-        self.joystick = pygame.joystick.Joystick(0)
-        self.joystick.init()
-        self.debug_button_last = 0
-        self.exit_button_last = 0
+        else:
+            gamepad = True
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+            self.debug_button_last = 0
+            self.exit_button_last = 0
 
-        self.done = False
+            self.done = False
 
         print(msgHeader + "Initialisation complete.")
 
     def splash_screen(self):
         self.screen.fill((0, 0, 0))
-        text = self.font.render("AUTONOMOUS VEHICLE TESTBED", True, (255, 255, 255))
+        text = self.font_general.render("AUTONOMOUS VEHICLE TESTBED", True, (255, 255, 255))
         self.screen.blit(text, self.default_text_position)
         pygame.display.flip()
 
@@ -65,15 +74,15 @@ class Display():
             pygame.draw.line(self.screen, (0, 255, 0), tl, bl, 5)
             pygame.draw.line(self.screen, (0, 255, 0), bl, br, 5)
             pygame.draw.line(self.screen, (0, 255, 0), br, tr, 5)
-            text = self.font.render("Calibrated successfully.", True, (0, 0, 0))
+            text = self.font_general.render("Calibrated successfully.", True, (0, 0, 0))
         else:
-            text = self.font.render("Calibrating camera perspective...", True, (0, 0, 0))
+            text = self.font_general.render("Calibrating camera perspective...", True, (0, 0, 0))
         self.screen.blit(text, self.default_text_position)
         pygame.display.flip()
 
     def connecting_screen(self):
         self.screen.fill((255, 255, 255))
-        text = self.font.render("Connecting to cars...", True, (0, 0, 0))
+        text = self.font_general.render("Connecting to cars...", True, (0, 0, 0))
         self.screen.blit(text, self.default_text_position)
         pygame.display.flip()
 
@@ -86,23 +95,45 @@ class Display():
         for agent in agents:
             pygame.draw.rect(self.screen, (0, 0, 0),
                              Rect(xOffset, top, cellWidth, cellHeight), 4)
-            id = self.font.render(str(agent.ID), True, (0, 0, 0))
+            id = self.font_general.render(str(agent.ID), True, (0, 0, 0))
             rect = id.get_rect(center=(xOffset + cellWidth / 2, top - cellHeight / 10))
             self.screen.blit(id, rect)
             xOffset += cellWidth + int(DISPLAY_WIDTH / 14)
 
-        text = self.font.render("Place each car in its cell.", True, (0, 0, 0))
+        text = self.font_general.render("Place each car in its cell.", True, (0, 0, 0))
         self.screen.blit(text, self.default_text_position)
         pygame.display.flip()
 
     # Create image from raw world data.
-    def generate_image(self, world_data):
-        if self.background_image is None:
-            raw_img = world_data["map"]
-            scale_factor = DISPLAY_WIDTH / raw_img.get_rect().size[0]
-            scaled_img = pygame.transform.rotozoom(raw_img, 0, scale_factor)
-            self.background_image = scaled_img
+    def generate_image(self, world_data, laps):
+        raw_img = world_data["map"]
+        scale_factor = DISPLAY_WIDTH / raw_img.get_rect().size[0]
+        scaled_img = pygame.transform.rotozoom(raw_img, 0, scale_factor)
+        self.background_image = scaled_img
         self.screen.blit(self.background_image, (0, 0))
+        dt = datetime.datetime(2019, 1, 1) # Parameters don't matter, just used to get current date/time
+        dt = dt.now().time() # Get current time
+        current_time = datetime.timedelta(seconds=dt.second, milliseconds=dt.microsecond/1000, 
+                                        minutes=dt.minute, hours=dt.hour)
+        time_elapsed = current_time - laps[0]
+        if self.race_complete and self.finish_time == None:
+            self.finish_time = time_elapsed # Store finish time
+            time_text = self.font_timer.render(str(self.finish_time), True, (255, 0, 0))
+            self.screen.blit(time_text, (800, 50))
+        elif self.race_complete:
+            time_text = self.font_timer.render(str(self.finish_time), True, (255, 0, 0))
+            self.screen.blit(time_text, (800, 50))
+        else:
+            time_text = self.font_timer.render(str(time_elapsed), True, (255, 0, 0)) # Render elapsed time on screen
+            self.screen.blit(time_text, (800, 50))
+        lap_y = 90 # Y coordinate for lap time text placement
+        lap_count = 1
+        for lap in laps[1:]:
+            lap_text = self.font_timer2.render('Lap '+str(lap_count)+' - '+str(lap), True, (255, 0, 0))
+            self.screen.blit(lap_text, (825, lap_y))
+            lap_y += 25
+            lap_count += 1
+        
         if self.DEBUG:
             yOffset = 0
             for vehicle in world_data['vehicles']:
@@ -115,40 +146,57 @@ class Display():
                     angleLine = (
                     pos[0] + 200 * math.cos(math.radians(angle)), pos[1] + 200 * math.sin(math.radians(angle)))
                     pygame.draw.line(self.screen, (0, 0, 0), pos, angleLine, 5)
-                    text = self.font.render(str(vehicle.owner.ID) + ": " + str(pos) + ", " + str(angle), True,
+                    text = self.font_general.render(str(vehicle.owner.ID) + ": " + str(pos) + ", " + str(angle), True,
                                             (0, 0, 0))
                     self.screen.blit(text, (50, yOffset))
                     yOffset += 30
-                    marker = self.font.render(str(vehicle.owner.ID), True, (0, 0, 0))
+                    marker = self.font_general.render(str(vehicle.owner.ID), True, (0, 0, 0))
                     self.screen.blit(marker, pos)
                 except Exception as e:
                     print(str(e))
 
     def error_message(self, message):
         self.screen.fill((0, 0, 0))
-        text = self.font.render(message, True, (255, 0, 0))
+        text = self.font_general.render(message, True, (255, 0, 0))
         self.screen.blit(text, self.default_text_position)
         pygame.display.flip()
 
     def handle_input(self):
         for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            if event.type == QUIT:
                 self.done = True
-        if self.joystick.get_button(8) and not self.debug_button_last:
-            self.DEBUG = not self.DEBUG
-        if self.joystick.get_button(1) and not self.exit_button_last:
-            self.done = True
-        self.debug_button_last = self.joystick.get_button(8)
-        self.exit_button_last = self.joystick.get_button(1)
+            elif event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
+                if (event.mod & pygame.KMOD_SHIFT):
+                    pygame.quit()
+                    quit()
+                    print(msgHeader+"Keyboard interrupt, exiting program")
+                    sys.exit() # A little inelegant but it works; skips terminal messages in main.py shutdown
+                else: self.done = True
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                if (event.mod & pygame.KMOD_SHIFT):
+                    self.race_complete = True
+                else: self.lap = True 
+        if gamepad:
+            if self.joystick.get_button(8) and not self.debug_button_last:
+                self.DEBUG = not self.DEBUG
+            if self.joystick.get_button(1) and not self.exit_button_last:
+                self.done = True
+            self.debug_button_last = self.joystick.get_button(8)
+            self.exit_button_last = self.joystick.get_button(1)
+        
 
     # Update the world display.
-    def update(self, world_data):
+    def update(self, world_data, start_time):
         self.handle_input()
-        if self.done: return
-        self.generate_image(world_data)
+        if self.done:
+            return
+        self.generate_image(world_data, start_time)
         pygame.display.flip()
 
-
+    # Clear the event queue to stop input buffering
+    def clear_events(self):
+        pygame.event.clear()
+        
     """
     My apologies for the following code! It was done in a hurry post-exams to try to
     tie everything together for you guys. I definitely advise doing a proper menu if
@@ -157,6 +205,7 @@ class Display():
     """
 
     def menu(self):
+    
         # Get maps from folder.
         maps = []
         for folder in listdir(MAPS_DIR):
@@ -197,7 +246,7 @@ class Display():
         if not strategies:
             print(msgHeader + "Error: No strategies in the strategy folder.")
             return False
-
+    
         # Get cars.
         cars = []
         file = csv.DictReader(open(os.path.join(ZENWHEELS_DIR, 'cars.csv')))
@@ -236,64 +285,108 @@ class Display():
         exit_button.is_exit = True
         buttons.append(exit_button)
 
-
         # Menu navigation.
         hovering_on = 0 # Make first button hovered over by default.
         prev_direction = (0, 0)
         prev_abxy = [0, 0, 0, 0]
         start = False
         exit = False
+        UI = False
         while not start and not exit:
             self.screen.fill((0, 0, 0))
             for event in pygame.event.get():
-                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                    return False
+                if event.type == QUIT or event.type == KEYDOWN:
+                    if (event.mod & pygame.KMOD_SHIFT) and event.key == K_ESCAPE:
+                        pygame.quit() # SHIFT + ESC will immediately quit the program
+                        quit()
+                        return False
+                        
+                    if event.key == K_RETURN: # ENTER functions the same as gamepad A
+                        buttons[hovering_on].A()
+                        if buttons[hovering_on].is_start:
+                            start = True
+                        elif buttons[hovering_on].is_exit:
+                            exit = True
+                        elif buttons[hovering_on].is_UI:
+                            UI = True
+                    elif event.key == K_ESCAPE: # ESC functions the same as gamepad B
+                        buttons[hovering_on].B()
+                        
+                    elif event.key == K_BACKSPACE: # BACKSPACE functions the same as gamepad X
+                        buttons[hovering_on].X()
+                        
+                    elif event.key == K_SPACE: # SPACE functions the same as gamepad Y
+                        buttons[hovering_on].Y()
+                    # Enable arrow keys as directional inputs
+                    elif event.key == K_DOWN and hovering_on < len(buttons) - 1:
+                        if buttons[hovering_on].is_start:
+                            pass
+                        else:
+                            hovering_on += 1
+                    
+                    elif event.key == K_UP and hovering_on > 0:
+                        if buttons[hovering_on].is_exit:
+                            pass
+                        else:
+                            hovering_on -= 1
+                    
+                    elif event.key == K_LEFT:
+                        if buttons[hovering_on].is_exit:
+                            hovering_on -= 1
+                        else:
+                            buttons[hovering_on].previous()
+                    
+                    elif event.key == K_RIGHT:
+                        if buttons[hovering_on].is_start:
+                            hovering_on += 1
+                        buttons[hovering_on].next()
+                        
+            if gamepad:
+                direction = self.joystick.get_hat(0)
+                A = self.joystick.get_button(0)
+                B = self.joystick.get_button(1)
+                X = self.joystick.get_button(2)
+                Y = self.joystick.get_button(3)
+                abxy = [A, B, X, Y]
 
-            direction = self.joystick.get_hat(0)
-            A = self.joystick.get_button(0)
-            B = self.joystick.get_button(1)
-            X = self.joystick.get_button(2)
-            Y = self.joystick.get_button(3)
-            abxy = [A, B, X, Y]
-
-            if direction == prev_direction:
-                pass
-            elif direction == (0, -1) and hovering_on < len(buttons) - 1:
-                if buttons[hovering_on].is_start:
+                if direction == prev_direction:
                     pass
-                else:
-                    hovering_on += 1
-            elif direction == (0, 1) and hovering_on > 0:
-                if buttons[hovering_on].is_exit:
-                    pass
-                else:
-                    hovering_on -= 1
-            elif direction == (-1, 0):
-                if buttons[hovering_on].is_exit:
-                    hovering_on -= 1
-                else:
-                    buttons[hovering_on].previous()
-            elif direction == (1, 0):
-                if buttons[hovering_on].is_start:
-                    hovering_on += 1
-                buttons[hovering_on].next()
-            prev_direction = direction
+                elif direction == (0, -1) and hovering_on < len(buttons) - 1:
+                    if buttons[hovering_on].is_start:
+                        pass
+                    else:
+                        hovering_on += 1
+                elif direction == (0, 1) and hovering_on > 0:
+                    if buttons[hovering_on].is_exit:
+                        pass
+                    else:
+                        hovering_on -= 1
+                elif direction == (-1, 0):
+                    if buttons[hovering_on].is_exit:
+                        hovering_on -= 1
+                    else:
+                        buttons[hovering_on].previous()
+                elif direction == (1, 0):
+                    if buttons[hovering_on].is_start:
+                        hovering_on += 1
+                    buttons[hovering_on].next()
+                prev_direction = direction
 
-            if prev_abxy != [0,0,0,0]:
-                pass
-            elif A:
-                buttons[hovering_on].A()
-                if buttons[hovering_on].is_start:
-                    start = True
-                elif buttons[hovering_on].is_exit:
-                    exit = True
-            elif B:
-                buttons[hovering_on].B()
-            elif X:
-                buttons[hovering_on].X()
-            elif Y:
-                buttons[hovering_on].Y()
-            prev_abxy = abxy
+                if prev_abxy != [0,0,0,0]:
+                    pass
+                elif A:
+                    buttons[hovering_on].A()
+                    if buttons[hovering_on].is_start:
+                        start = True
+                    elif buttons[hovering_on].is_exit:
+                        exit = True
+                elif B:
+                    buttons[hovering_on].B()
+                elif X:
+                    buttons[hovering_on].X()
+                elif Y:
+                    buttons[hovering_on].Y()
+                prev_abxy = abxy
 
             for i, button in enumerate(buttons):
                 if i == hovering_on:
@@ -307,12 +400,12 @@ class Display():
                 if car["Enabled"]:
                     enabled_cars.append(car["ID"])
             if enabled_cars:
-                text_render = self.font.render("Enabled Cars:", True, (255,255,255))
+                text_render = self.font_general.render("Enabled Cars:", True, (255,255,255))
                 text_rect = text_render.get_rect(center=(DISPLAY_WIDTH / 6, DISPLAY_HEIGHT / 2))
                 self.screen.blit(text_render, text_rect)
                 yOffset = DISPLAY_HEIGHT / 30
                 for ID in enabled_cars:
-                    car_text_render = self.font.render(ID, True, (100,120,255))
+                    car_text_render = self.font_general.render(ID, True, (100,120,255))
                     car_text_rect = car_text_render.get_rect(center=(DISPLAY_WIDTH / 6, (DISPLAY_HEIGHT / 2) + yOffset))
                     self.screen.blit(car_text_render, car_text_rect)
                     yOffset += DISPLAY_HEIGHT / 30
@@ -320,8 +413,10 @@ class Display():
             pygame.display.flip()
 
         if start:
+            print(msgHeader+"Rebuilding scenario_config")
             scenario_config = {}
             scenario_config["Map"] = maps[map_button.current]
+            print(msgHeader+"scenario_config map = "+scenario_config["Map"]["Name"])
             scenario_config["Active Cars"] = []
             for car in cars:
                 if car["Enabled"]:
@@ -329,6 +424,7 @@ class Display():
             return scenario_config
         else:
             pygame.quit()
+            quit()
             return False
 
 
@@ -356,10 +452,8 @@ class Button:
         self.right_arrow = pygame.transform.scale(pygame.image.load("media/right-arrow.png"),
                                              (int(self.w / 2), int(self.h / 2)))
         
-        #Initilaise UI button, this image will be a small size, since divided by 2
-        self.UI = pygame.transform.scale(pygame.image.load("media/UI.png"),
-                                             (int(self.w / 2), int(self.h / 2 )))
-
+        self.UI_button = pygame.transform.scale(pygame.image.load("media/UI.png"),
+                                             (int(self.w), int(self.h)))
 
         self.hover = False
 
@@ -412,8 +506,7 @@ class Button:
                 screen.blit(self.right_arrow, (self.x + self.w, self.y + self.h / 4))
 
         if self.maps:
-    #Add UI image to the top corner
-            screen.blit(self.UI, (self.x - self.w /1, self.y - self.h / 4))
+            screen.blit(self.UI_button, (self.x  - self.w, (self.y + 150)+ self.h))
             button_img = pygame.transform.scale(self.maps[self.current]["Image"], (self.w, self.h))
             screen.blit(button_img, (self.x, self.y))
             if self.hover:
@@ -462,6 +555,7 @@ class Button:
         elif self.is_exit:
             pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.w, self.h), 0)
             self.text = "Exit"
+            
 
         if (self.text != ""):
             if self.cars and self.cars[self.current]["Colour"] == "white":
